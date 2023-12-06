79c3a449eded402d87386987ac85e3ff6852d04b
Augustus Greenwood <gusbgreenwood@gmail.com>
2023-12-06 10:28:31 -0600

    fixed check

--- src/main.c
+++ src/main.c
@@ -2,11 +2,14 @@
 #include "Result.h"
 
 int main() {
-  Device device = {0};
-  openDevice(&device);
-  unsigned char response[64];
-  unsigned char command[64] = "HSPD";
-  sendCommandGetResponse(device, command, response);
-  closeDevice(device);
-  return 0;
+    Device device = {0};
+    openDevice(&device);
+    if (device.handle == NULL) {
+        return 0;
+    }
+    unsigned char response[64];
+    unsigned char command[64] = "HSPD";
+    sendCommandGetResponse(device, command, response);
+    closeDevice(device);
+    return 0;
 }
